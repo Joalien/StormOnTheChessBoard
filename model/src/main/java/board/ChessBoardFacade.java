a75e3c960ab6a5e88c76b7489db5d1a0fffe1b38
Josquin Cornec <josquin.cornec+geek@kubys.fr>
2023-04-01 21:28:46 +0200

    simplify pom

--- model/src/main/java/board/ChessBoardFacade.java
+++ model/src/main/java/board/ChessBoardFacade.java
@@ -5,17 +5,16 @@ import piece.Piece;
 
 public class ChessBoardFacade {
 
-    private final ChessBoard chessBoard;
-    public ChessBoardFacade(ChessBoard chessBoard) {
-        this.chessBoard = chessBoard;
+    private ChessBoard chessBoard;
+    public ChessBoardFacade() {
+    }
+
+    public void startGame() {
+        this.chessBoard = ChessBoard.createWithInitialState();
     }
 
     public boolean tryToMove(String from, String to) {
         return chessBoard.tryToMove(from, to);
-    }
-
-    public boolean tryToMove(Piece piece, String positionToMoveOn) {
-        return chessBoard.tryToMove(piece, positionToMoveOn);
     }
 
     public boolean tryToPlayCard(SCCard card) {
