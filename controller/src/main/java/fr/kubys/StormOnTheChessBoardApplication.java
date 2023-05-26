b459388e011757229c3da619e9d64a8043c7e934
Josquin Cornec <josquin.cornec+geek@kubys.fr>
2023-05-26 15:22:10 +0200

    fix start of the application outside of IDE

--- controller/src/main/java/fr/kubys/StormOnTheChessBoardApplication.java
+++ controller/src/main/java/fr/kubys/StormOnTheChessBoardApplication.java
@@ -1,9 +1,12 @@
 package fr.kubys;
 
+import fr.kubys.repository.ChessBoardRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {"fr.kubys"})
+@SpringBootApplication
+@Import(ChessBoardRepositoryImpl.class) // FIXME Try to generify me
 public class StormOnTheChessBoardApplication {
 
     public static void main(String[] args) {
